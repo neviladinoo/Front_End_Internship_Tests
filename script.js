3a0449033d5a9022c9e3d6561db2ad2caf95e1c4
Your Name <your.email@example.com>
2025-01-23 22:48:06 +0100

    Krijimi i projektit dhe shtimi i skedarëve

---
+++ script.js
@@ -0,0 +1,36 @@
+const track = document.querySelector('.carousel-container');
+const prevButton = document.querySelector('.carousel-arrow.left');
+const nextButton = document.querySelector('.carousel-arrow.right');
+const dots = document.querySelectorAll('.carousel-dot');
+let currentIndex = 0;
+
+const updateCarousel = () => {
+    const itemWidth = track.querySelector('.carousel-item').clientWidth;
+    track.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[currentIndex].classList.add('active');
+};
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % track.children.length;
+    updateCarousel();
+});
+
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + track.children.length) % track.children.length;
+    updateCarousel();
+});
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        currentIndex = index;
+        updateCarousel();
+    });
+});
+
+window.addEventListener('resize', updateCarousel);
+
+
+updateCarousel();
+
